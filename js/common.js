4051f8bf638633a6b2baba0154810a6f58f7a0dc
Kyunghoon Han <bin8639@naver.com>
2020-09-11 10:15:10 +0900

    js 로고부분 삭제

--- js/common.js
+++ js/common.js
@@ -9,10 +9,5 @@
         $('#secBox').load(url)
     })
 
-    $('.logoNav > h1 > a').on('click', function(e){
-        e.preventDefault()
-        var url = $(this).attr('href')
-        $('#secContainer').remove()
-        $('#secBox').load(url)
-    })
+
 })(jQuery)
