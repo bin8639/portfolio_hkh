08fa9818d11a2e1c241b1c75bd945615b6453752
Kyunghoon Han <bin8639@naver.com>
2020-09-11 18:18:04 +0900

    고탑버튼 추가

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-(function($){
+(function ($) {
     //메인 페이지 연결
     $('#secBox').load('main.html')
 
@@ -11,21 +11,21 @@
     })
 
     // 탑메뉴 클릭시 대표 메뉴 연결
-    $('.depth1 > li:nth-child(1) > a').on('click', function(e){
+    $('.depth1 > li:nth-child(1) > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('secContainer').remove()
         $('#secBox').load(url)
     })
 
-    $('.depth1 > li:nth-child(2) > a').on('click', function(e){
+    $('.depth1 > li:nth-child(2) > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('secContainer').remove()
         $('#secBox').load(url)
     })
 
-    $('.depth1 > li:nth-child(3) > a').on('click', function(e){
+    $('.depth1 > li:nth-child(3) > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('secContainer').remove()
@@ -33,15 +33,15 @@
     })
 
     // 인사말 페이지 연결
-    $('.com_info > li:nth-child(1) > a').on('click', function(e){
+    $('.com_info > li:nth-child(1) > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('#secContainer').remove()
         $('#secBox').load(url)
     })
-    
+
     // 찾아오시는 길 연결
-    $('.com_info > li:nth-child(2) > a').on('click', function(e){
+    $('.com_info > li:nth-child(2) > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('#secContainer').remove()
@@ -49,7 +49,7 @@
     })
 
     // 사업분야 연결
-    $('.buss_info > li > a').on('click', function(e){
+    $('.buss_info > li > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('#secContainer').remove()
@@ -57,7 +57,7 @@
     })
 
     // 제품소개1 연결
-    $('.prod_info > li:nth-child(1) > a').on('click', function(e){
+    $('.prod_info > li:nth-child(1) > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('#secContainer').remove()
@@ -66,20 +66,43 @@
 
     // 탑메뉴 호버시 슬라이드 효과
     $('.nav').hover(
-        function(){
+        function () {
             $(this).find('.depth2').stop().slideDown(400)
             $(this).find('.decoBox').stop().slideDown(400)
         },
-        function(){
+        function () {
             $(this).find('.depth2').stop().slideUp(400)
             $(this).find('.decoBox').stop().slideDown(400)
         }
     )
 
-    $('.logoNav').hover( 
-        function(){ $(this).find('.decoBox').stop().slideDown(400) },
-        function(){ $(this).find('.decoBox').stop().slideUp(400) }
-        )
-        
+    $('.logoNav').hover(
+        function () {
+            $(this).find('.decoBox').stop().slideDown(400)
+        },
+        function () {
+            $(this).find('.decoBox').stop().slideUp(400)
+        }
+    )
+
+    var sct = 0;
+    var winHeight = $(window).height();
+    
+    // gotop버튼 생기기
+    $(window).scroll(function(){
+        sct = $(this).scrollTop();
+        if ( sct >= winHeight/2 ) {
+            $('.goTop').addClass('on');
+        } else {
+            $('.goTop').removeClass('on');
+        }
+    });
+
+    // goTop버튼 누르면 오르기
+    $('.goTop').on('click', function(){
+        $('body, html').stop().animate({
+            scrollTop: 0
+        }, 800 ,'linear');
+    });
 
 })(jQuery)
