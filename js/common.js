91faffd64cb18baba667e608bd5c5817abca16ff
Kyunghoon Han <bin8639@naver.com>
2020-09-11 16:55:19 +0900

    연결

--- js/common.js
+++ js/common.js
@@ -1,7 +1,8 @@
 (function($){
-    
+    //메인 페이지 연결
     $('#secBox').load('main.html')
 
+    //로그인 페이지 연결
     $('.login > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
@@ -9,6 +10,61 @@
         $('#secBox').load(url)
     })
 
+    // 탑메뉴 클릭시 대표 메뉴 바로 연결
+    $('.depth1 > li:nth-child(1) > a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('secContainer').remove()
+        $('#secBox').load(url)
+    })
+
+    $('.depth1 > li:nth-child(2) > a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('secContainer').remove()
+        $('#secBox').load(url)
+    })
+
+    $('.depth1 > li:nth-child(3) > a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('secContainer').remove()
+        $('#secBox').load(url)
+    })
+
+    // 서브페이지1-1 연결
+    $('.com_info > li:nth-child(1) > a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#secContainer').remove()
+        $('#secBox').load(url)
+    })
+    
+    // 서브페이지1-2 연결
+    $('.com_info > li:nth-child(2) > a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#secContainer').remove()
+        $('#secBox').load(url)
+    })
+
+    // 서브페이지2 연결
+    $('.buss_info > li > a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#secContainer').remove()
+        $('#secBox').load(url)
+    })
+
+    // 서브페이지3 연결
+    $('.prod_info > li:nth-child(1) > a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#secContainer').remove()
+        $('#secBox').load(url)
+    })
+
+    // 탑메뉴 호버시 슬라이드 효과
     $('.nav').hover(
         function(){
             $(this).find('.depth2').stop().slideDown(500)
