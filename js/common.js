bb4a76314f456d9f7edbc777779e0dc5e4122532
Kyunghoon Han <bin8639@naver.com>
2020-09-16 16:50:08 +0900

    qna 게시판

--- js/common.js
+++ js/common.js
@@ -69,6 +69,14 @@
         $('#secBox').load(url)
     })
 
+    // Q&A 연결
+    $('.qna_info > li:nth-child(1) > a').on('click', function (e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#secContainer').remove()
+        $('#secBox').load(url)
+    })
+
     // 공지사항 연결
     $('.cs_info > li:nth-child(1) > a').on('click', function(e){
         e.preventDefault()
