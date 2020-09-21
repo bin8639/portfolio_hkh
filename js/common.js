b05aa4286d3fb67fe9c97ad7b9c790cbdc1c921a
Kyunghoon Han <bin8639@naver.com>
2020-09-21 16:38:33 +0900

    ㅏㅏ

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 (function ($) {
     // 페이지 로딩 창
-    $(window).load(function(){
+    $(window).load(function () {
         $('.loading').delay(1000).fadeOut(1000)
     })
 
@@ -43,7 +43,7 @@
         $('#secContainer').remove()
         $('#secBox').load(url)
     })
-    
+
     $('.depth1 > li:nth-child(5) > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
@@ -84,7 +84,7 @@
     })
 
     // Q&A 연결
-    $('.qna_info > li:nth-child(1) > a').on('click', function (e){
+    $('.qna_info > li:nth-child(1) > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('#secContainer').remove()
@@ -92,7 +92,7 @@
     })
 
     // 공지사항 연결
-    $('.cs_info > li:nth-child(1) > a').on('click', function(e){
+    $('.cs_info > li:nth-child(1) > a').on('click', function (e) {
         e.preventDefault()
         var url = $(this).attr('href')
         $('#secContainer').remove()
@@ -113,11 +113,11 @@
 
     var sct = 0;
     var winHeight = $(window).height();
-    
+
     // gotop버튼 생기기
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         sct = $(this).scrollTop();
-        if ( sct >= winHeight/2 ) {
+        if (sct >= winHeight / 2) {
             $('.goTop').addClass('on');
         } else {
             $('.goTop').removeClass('on');
@@ -125,10 +125,10 @@
     });
 
     // goTop버튼 누르면 오르기
-    $('.goTop').on('click', function(){
+    $('.goTop').on('click', function () {
         $('body, html').stop().animate({
             scrollTop: 0
-        }, 800 ,'linear');
+        }, 800, 'linear');
     });
 
 
